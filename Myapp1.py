b62cf370906135f206b0c5f26ddfc461edaa447e
Chutimasuk01 <chutima18092544@gmail.com>
2023-09-19 12:02:35 +0700

    commit

--- Myapp1.py
+++ Myapp1.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('Chutima')
-st.header('Chutima')
-st.subheader('Chutima')
+st.title('การต้นสอบเขียนเว็บด้วย Python')
+st.header('นางสาวชุติมา สุขสมัย')
+st.subheader('สาขาวิชาวิทยากรข้อมูล')
+st.markdown('---')
